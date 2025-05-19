ccee4abe43bd3dcaa46d5df557b021c6b3429579
Lodeme101 <lodeme101@gmail.com>
2025-05-19 09:35:04 +0900

    Fixed array issue in Dictionary.js and added final details

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,7 +11,7 @@ export default function Dictionary(props) {
   let [photos, setPhotos] = useState(null);
 
   function handleDictionResponse(response) {
-    setResults(response.data[0]);
+    setResults(response.data);
   }
 
   function handleImagesResponse(response) {
@@ -56,7 +56,7 @@ export default function Dictionary(props) {
             />
           </form>
           <div className="hint">
-            suggested words: sunset, wine, yoga, plant...
+            suggested words: fire, earth, water, air...
           </div>
         </section>
         <Results results={results} />
--- src/Meaning.js
+++ src/Meaning.js
@@ -5,8 +5,8 @@ import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      <h3>{props.meaning.definition}</h3>
+      <h3> {props.meaning.partOfSpeech}</h3>
+      <h4> {props.meaning.definition}</h4>
       <div className="example">{props.meaning.example}</div>
       <Synonyms synonyms={props.meaning.synonyms} />
     </div>
